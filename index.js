e40c54260e9f46de035c92b6ef2dae2be2eb075f
Bill <billfeng2002@gmail.com>
2021-08-02 13:52:23 -0700

    commit tree bd3bed1b9556fcf9a5c6e675508bf1e980f2cf8c

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string[0].toUpperCase()+string.substring(1,string.length)
+  }
+
+  static sanitize(string){
+    const regex = /[^a-zA-Z0-9\-\ \']/g;
+    return string.replaceAll(regex, "")
+  }
+
+  static titleize(string){
+    let strings=string.split(" ")
+    for(let i=0;i<strings.length; i++){
+      if(!"the a an but of and for at by from".includes(strings[i]))
+        strings[i]=this.capitalize(strings[i])
+    }
+    string=strings.join(" ")
+    return this.capitalize(string)
+  }
 }
